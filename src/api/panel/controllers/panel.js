c42ff7e85214716ce37b958728f213327f2341a3
Sergio Henrique <sergiohenriquecarvalho@hotmail.com>
2022-04-04 11:35:40 -0300

    feat: customization init

---
+++ src/api/panel/controllers/panel.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  panel controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::panel.panel');
