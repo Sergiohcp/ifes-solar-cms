1c1366b528024017ee02d41be1a3f76b3314e9a2
Sergio Henrique <sergiohenriquecarvalho@hotmail.com>
2022-04-04 19:46:43 -0300

    feat: wrapping find controller

--- src/api/panel/controllers/panel.js
+++ src/api/panel/controllers/panel.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::panel.panel');
+module.exports = createCoreController('api::panel.panel', ({ strapi }) => ({
+    async find() {
+
+      const data = await strapi.service('api::panel.panel').find({
+        populate: ['producer_image', 'datasheet']
+      });
+
+      const dataMapped = data.results.map(item => ({
+          ...item,
+          producer_image: item.producer_image.url,
+          datasheet: item.datasheet.url
+      }))
+  
+      return { data: dataMapped };
+    },
+  }));
